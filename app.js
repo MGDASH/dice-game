3ce45f78fdf807f6a636c6bab062f9ab70e5162e
Miga <mgsdash@gmail.com>
2020-09-26 22:50:56 -0700

    shoog shiddeg bolgoh

--- app.js
+++ app.js
@@ -6,17 +6,19 @@ var scores = [0, 0];
 // Toglogchii eeljindee tsugluulaj bgaa onoog hadgalah hubisagch
 var roundScore = 0;
 // Shoonii ali talaaraa buusniig hadgalah hubisagch heregtei, 1-6 gesen utgiig ene hubisagchid sanamsarguigeer uusgej ogno
-var dice = Math.floor(Math.random() * 6) + 1;
-
-// <div class="player-score" id="score-0">43</div>
-// window.document.querySelector("#score-0").textContent = dice;
-// document.querySelector("#score-1").textContent = dice;
+var diceNumber = Math.floor(Math.random() * 6) + 1;
 
 // Program ehlehed beltgee
-document.querySelector("#score-0").textContent = 0;
-document.querySelector("#score-1").textContent = 0;
-document.querySelector("#current-0").textContent = 0;
-document.querySelector("#current-1").textContent = 0;
-document.querySelector(".dice").style.display = "none";
+document.getElementById("score-0").textContent = "0";
+document.getElementById("score-1").textContent = "0";
+document.getElementById("current-0").textContent = "0";
+document.getElementById("current-1").textContent = "0";
 
-console.log("Шоо :" + dice);
+var diceDom = document.querySelector(".dice");
+diceDom.style.display = "none";
+document.querySelector(".btn-roll").addEventListener("click", function () {
+  var diceNumber = Math.floor(Math.random() * 6) + 1;
+
+  diceDom.style.display = "block";
+  diceDom.src = "dice-" + diceNumber + ".png";
+});
